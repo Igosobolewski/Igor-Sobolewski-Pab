636b6bbd16afcd0519c8e58c872fd9ffae757e72
Igosobolewski <igosobolewski@gmail.com>
2022-03-22 19:25:22 +0100

    Commit

--- Projekt2/index.ts
+++ Projekt2/index.ts
@@ -3,30 +3,25 @@ import {Request, Response} from 'express'
 
 const app = express()
 
-/*app.post('/:notes/:content/:createDate/:tags/:id', function (req: any, res: any){
-    let title: string
-    let content: string = req.params.content
-    const CreateDate = new Date();
-    let tags: string[]
-    let id: number = 0
-}*/
-
-let notes = [{
-    id: 1,
-    title: 'aa'
-}]
-
-let note = notes.find(note =>   )
-
 app.use(express.json())
 
-app.get('/', function (req: Request, res: Response) {
-  res.send('GET Hello World')
-})
-app.post('/', function (req: Request, res: Response) {
-  console.log(req.body) // e.x. req.body.title 
-  res.status(200).send('POST Hello World')
-})
+interface Note {
+  id?: number
+  title: string;
+  content: string;
+  createDate: string;
+  tags: string[];
+}
+
+const notes : Note[] =[
+  {
+    id: 1,
+    title: "test",
+    content: "this is a test note",
+    createDate: "rndDate",
+    tags: ["tag1"]
+  }
+]
 
 
 
