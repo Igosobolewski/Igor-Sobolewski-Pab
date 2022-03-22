3475be480d55211efe6ca4b0f5aa7e0866bd8ebe
Igosobolewski <igosobolewski@gmail.com>
2022-03-22 23:20:51 +0100

    PostPutDelete

--- Projekt2/index.ts
+++ Projekt2/index.ts
@@ -24,6 +24,7 @@ const notes : Note[] =[
 ]
 
 app.get('/:note/:id', function (req: Request, res: Response) {
+  
   const id = parseInt(req.params.id)
 
   if(notes.findIndex(note=>note.id == id)){
@@ -32,6 +33,47 @@ app.get('/:note/:id', function (req: Request, res: Response) {
     res.sendStatus(404).send("no object")
   }
 })
+
+app.post('/note', function (req: Request, res: Response) {
+  const data = new Date().toISOString()
+  const id = req.body.id == null? Date.now(): req.body.id
+  const newNote : Note =
+  {
+    id : id, 
+    title : req.body.title,
+    content : req.body.content,
+    createDate : data,
+    tags : req.body.tags
+  }
+  if(newNote.title!==null && newNote.content!==null)
+  {
+    notes.push(newNote);
+    console.log(req.body) 
+    res.sendStatus(201).send(newNote.id)
+  }else{
+    res.sendStatus(400).send("no title or content")
+  }
+  })
+
+  app.put('/note/:id', function (req: Request, res: Response) {
+    const id = parseInt(req.body.id)
+    if(notes.findIndex(note=>note.id == id)){
+      notes[notes.findIndex(note=>note.id == id)] = req.body;
+      res.sendStatus(200).send(notes.findIndex(note=>note.id == id))
+    }else{
+      res.sendStatus(404).send("no object")
+    }
+  })
+  
+  app.delete('/note/:id', function(req: Request, res: Response){
+    const id = parseInt(req.body.id)
+    if(notes.find(note=>note.id == id)){
+      res.sendStatus(200).send(notes.findIndex(note=>note.id == id))
+      notes.splice(notes.findIndex(note=>note.id == id),1)
+    }else{
+      res.sendStatus(404).send("no object")
+    }
+  })
 
 
 app.listen(3000)
