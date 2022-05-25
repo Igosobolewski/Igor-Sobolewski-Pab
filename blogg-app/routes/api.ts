d870a0c68b12b142fd969737766682390055d54c
Igosobolewski <99357157+Igosobolewski@users.noreply.github.com>
2022-05-25 11:15:23 +0200

    Delete blogg-app directory

--- blogg-app/routes/api.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-const testController = require('../controllers/api/test');
-
-router.get('/', testController);
-module.exports = router;
