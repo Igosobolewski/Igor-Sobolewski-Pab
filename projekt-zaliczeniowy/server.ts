e1fa87512d1695ab97fa4885154705fa919d152a
Igosobolewski <igosobolewski@gmail.com>
2022-05-31 18:53:07 +0200

    AppStructure

--- projekt-zaliczeniowy/server.ts
+++ projekt-zaliczeniowy/server.ts
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
 
-app.use(express.json());
+app.use(express.json())
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const DB = mongoose.connection;
+DB.on('err', (err: any) => console.log(err))
+DB.once('open', () => console.log('Connected to the database!'));
 
 app.get('/', (req: any, res: any) => {
     res.json('Hello World')
 })
+
 
 app.listen(3000);
